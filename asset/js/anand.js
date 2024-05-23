7541a13e2a5d43d36b86347fab178843ddf9971d
anand-2017 <167391600+anand-2017@users.noreply.github.com>
2024-05-23 09:29:09 +0530

    Initial commit

---
+++ asset/js/anand.js
@@ -0,0 +1,28 @@
+//STATEMENTS :-
+//---------------------------------------
+
+
+for (let i = 1; i <= 5; i++) {
+
+    console.log('king')
+    if (i == 3) {
+        continue
+    }
+    console.log(i)
+}
+console.log('google')
+
+console.log('----------------')
+
+for (let i = 1; i <= 5; i++) {
+
+    if (i == 3 || i == 5 || i == 2) {
+        continue
+    }
+    console.log('queen')
+    console.log(i)
+
+}
+console.log('facebook')
+
+
